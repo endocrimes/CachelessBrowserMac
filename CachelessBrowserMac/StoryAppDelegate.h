ddbdf64611feeeac39e5fca10132363953aa201e
Daniel Tomlinson <daniel.tomlinson11@gmail.com>
2012-06-12 22:04:16 +0100

    Initial

--- CachelessBrowserMac/StoryAppDelegate.h
+++ CachelessBrowserMac/StoryAppDelegate.h
@@ -7,9 +7,21 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 
+NSURLConnection *connection;
+NSMutableData* data;
+NSMutableArray *lines;
 @interface StoryAppDelegate : NSObject <NSApplicationDelegate>
 
+@property (nonatomic, retain) NSURLConnection *connection;
+@property (nonatomic, retain) NSMutableData* data;
+@property (nonatomic, retain) NSMutableArray *lines;
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *URL;
+- (IBAction)go:(id)sender;
+- (IBAction)refresh:(id)sender;
+
+@property (weak) IBOutlet WebView *web;
 
 @end
